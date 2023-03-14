ff3d288945b37519ce3198c4c008f4fa1bc10b6a
Sangwoon Park <nbiosupr@gmail.com>
2023-03-14 23:33:00 +0900

    init

---
+++ src/main/java/com/example/ffmpegbuildpackex/FfmpegBuildpackExApplication.java
@@ -0,0 +1,13 @@
+package com.example.ffmpegbuildpackex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmpegBuildpackExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfmpegBuildpackExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ffmpegbuildpackex/FfmpegBuildpackExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ffmpegbuildpackex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmpegBuildpackExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
