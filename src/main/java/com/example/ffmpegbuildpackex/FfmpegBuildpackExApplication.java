5a3b35de3e942e858ea555ba26d739ece974c3ec
Sangwoon Park <nbiosupr@gmail.com>
2023-03-16 21:06:13 +0900

    RestController로 수정

--- src/main/java/com/example/ffmpegbuildpackex/FfmpegBuildpackExApplication.java
+++ src/main/java/com/example/ffmpegbuildpackex/FfmpegBuildpackExApplication.java
@@ -6,9 +6,10 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class FfmpegBuildpackExApplication {
 
